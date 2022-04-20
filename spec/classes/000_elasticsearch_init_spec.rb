f31c9588333969c143982a30cb43c7c6ca126bc3
Pedro González Serrano <niteman.es@gmail.com>
2022-04-20 13:04:44 +0200

    Add support for SLM policies

--- spec/classes/000_elasticsearch_init_spec.rb
+++ spec/classes/000_elasticsearch_init_spec.rb
@@ -510,6 +510,8 @@ describe 'elasticsearch', type: 'class' do
             'snapshot_repository'
           when 'ilm_policies'
             'ilm_policy'
+          when 'slm_policies'
+            'slm_policy'
           else
             string[0..-2]
           end
@@ -525,6 +527,7 @@ describe 'elasticsearch', type: 'class' do
             'foo' => { 'source' => 'puppet:///path/to/foo.groovy' }
           },
           'snapshot_repositories' => { 'backup' => { 'location' => '/backups' } },
+          'slm_policies' => { 'foo' => { 'content' => {} } },
           'templates' => { 'foo' => { 'content' => {} } },
           'users' => { 'elastic' => { 'password' => 'foobar' } },
           'index_templates' => { 'foo' => { 'content' => {} } },
